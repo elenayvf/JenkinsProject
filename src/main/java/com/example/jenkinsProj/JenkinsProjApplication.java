7754e26a3f6ea1daae4038940be5b6b2528e7dbe
Elena Vega-Fujioka <elenavegafujioka@yahoo.com>
2021-06-03 16:15:49 -0700

    take away mapping

--- src/main/java/com/example/jenkinsProj/JenkinsProjApplication.java
+++ src/main/java/com/example/jenkinsProj/JenkinsProjApplication.java
@@ -14,8 +14,4 @@ public static void main(String[] args) {
 SpringApplication.run(JenkinsProjApplication.class, args);
 }
 
-@GetMapping("/hello")
-public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-return String.format("Hello %s!", name);
-}
 }
